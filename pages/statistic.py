cf623021d1fa434316b98cae35e9dbe72a3c57b7
kittichokv18 <kittichokv18@gmail.com>
2024-03-28 15:47:46 +0700

    Update statistic.py

--- pages/statistic.py
+++ pages/statistic.py
@@ -39,23 +39,17 @@ st.write(dt.head(10))
 
 st.subheader("max")
 st.write('ค่ามากที่สุด')
-# หาค่าสูงสุดของทุกคอลัมน์ยกเว้น 'Class'
 max_values = dt.drop('Class', axis=1).max()
 
-# แสดงค่าสูงสุดของแต่ละคอลัมน์
 for column, max_value in max_values.items():
     st.write(f"ค่าสูงสุดของคอลัมน์ {column}: {max_value}")
 
-# หาข้อมูลที่มีค่า Solidity มากที่สุด
 max_solidity_row = dt.loc[dt['Solidity'].idxmax()]
-
-# แสดงข้อมูลของค่า Solidity มากที่สุดและคลาสที่เกี่ยวข้อง
-st.subheader("ข้อมูลมากที่สุดของ Solidity และคลาสที่เกี่ยวข้อง")
+st.subheader("ข้อมูลมากที่สุดของ Solidity")
 st.write("ค่า Solidity มากที่สุด:", max_solidity_row['Solidity'])
 st.write("คลาส:", max_solidity_row['Class'])
 
 max_AspectRation_row = dt.loc[dt['AspectRation'].idxmax()]
-# แสดงข้อมูลของค่า AspectRation มากที่สุดและคลาสที่เกี่ยวข้อง
-st.subheader("ข้อมูลมากที่สุดของ AspectRation และคลาสที่เกี่ยวข้อง")
+st.subheader("ข้อมูลมากที่สุดของ AspectRation")
 st.write("ค่า AspectRation มากที่สุด:", max_AspectRation_row['AspectRation'])
 st.write("คลาส:", max_AspectRation_row['Class'])
