622f7c4c27e24340b8b490a1f7d6ef228d60481d
kittichokv18 <kittichokv18@gmail.com>
2024-03-28 14:37:26 +0700

    .

--- home.py
+++ home.py
@@ -1,4 +1,21 @@
 import streamlit as st
+from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie_spinner
+import requests
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+lottie_url_hello = "https://lottie.host/377a24ee-4e19-48c9-8a2f-fcada962c70a/oax9riei3m.json"
+lottie_hello = load_lottieurl(lottie_url_hello)
+
+
+#การเรียกใช้งาาน lottie file
+st_lottie(lottie_hello, key="hello")
+st.balloons()
 
 st.page_link("home.py", label="หน้าแรก", icon="🏠")
 st.page_link("pages/statistic.py", label="การนำเสนอข้อมลด้วยสถิติ", icon="1️⃣")
